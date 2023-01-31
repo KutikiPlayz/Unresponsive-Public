5c3b9ca9c019308bf2c104c81bb7f325c0e9af90
KutikiPlayz <kutikiplayz@gmail.com>
2023-01-31 14:47:12 -0700

    the shit

---
+++ source/options/OptionsState.hx
@@ -0,0 +1,173 @@
+package options;
+
+import openfl.filters.ColorMatrixFilter;
+import openfl.filters.BitmapFilter;
+import openfl.filters.DisplacementMapFilterMode;
+import openfl.geom.Point;
+import flixel.FlxCamera;
+import openfl.filters.DisplacementMapFilter;
+#if desktop
+import Discord.DiscordClient;
+#end
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.utils.Assets;
+import flixel.FlxSubState;
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.util.FlxSave;
+import haxe.Json;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxTimer;
+import flixel.input.keyboard.FlxKey;
+import flixel.graphics.FlxGraphic;
+import Controls;
+
+using StringTools;
+
+class OptionsState extends MusicBeatState
+{
+	var options:Array<String> = ['Controls', 'Adjust Delay', 'Graphics', 'Visuals and UI', 'Gameplay'];
+	private var grpOptions:FlxTypedGroup<Alphabet>;
+	private static var curSelected:Int = 0;
+	public static var menuBG:FlxSprite;
+
+	var cam:FlxCamera;
+
+	public static var optionsDisplacementFilter:DisplacementMapFilter;
+	var shaders = new Array<BitmapFilter>();
+
+	function openSelectedSubstate(label:String) {
+		switch(label) {
+			case 'Note Colors':
+				openSubState(new options.NotesSubState());
+			case 'Controls':
+				openSubState(new options.ControlsSubState());
+			case 'Graphics':
+				openSubState(new options.GraphicsSettingsSubState());
+			case 'Visuals and UI':
+				openSubState(new options.VisualsUISubState());
+			case 'Gameplay':
+				openSubState(new options.GameplaySettingsSubState());
+			case 'Adjust Delay':
+				LoadingState.loadAndSwitchState(new options.NoteOffsetState());
+		}
+	}
+
+	var selectorLeft:Alphabet;
+	var selectorRight:Alphabet;
+
+	override function create() {
+		cam = new FlxCamera();
+		FlxG.cameras.reset(cam);
+		FlxG.cameras.setDefaultDrawTarget(cam, true);
+
+		var red:Array<Float> = [
+			1, 0, 0, 0, 0,
+			0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0,
+			0, 0, 0, 1, 0,
+		];
+		shaders.push(new ColorMatrixFilter(red));
+
+		optionsDisplacementFilter = new DisplacementMapFilter(openfl.Assets.getBitmapData("assets/images/staticMap.png"), new Point(0, 0), 1, 1, 1, 1, DisplacementMapFilterMode.COLOR, 1, 0);
+		shaders.push(optionsDisplacementFilter);
+
+		if(!FlxG.save.data.defeatedFatal)
+			cam.setFilters(shaders);
+
+		#if desktop
+		DiscordClient.changePresence("Options Menu", null);
+		#end
+
+		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
+		bg.color = 0xFFea71fd;
+		bg.updateHitbox();
+
+		bg.screenCenter();
+		bg.antialiasing = ClientPrefs.globalAntialiasing;
+		add(bg);
+
+		grpOptions = new FlxTypedGroup<Alphabet>();
+		add(grpOptions);
+
+		for (i in 0...options.length)
+		{
+			var optionText:Alphabet = new Alphabet(0, 0, options[i], true);
+			optionText.screenCenter();
+			optionText.y += (100 * (i - (options.length / 2))) + 50;
+			grpOptions.add(optionText);
+		}
+
+		selectorLeft = new Alphabet(0, 0, '>', true);
+		add(selectorLeft);
+		selectorRight = new Alphabet(0, 0, '<', true);
+		add(selectorRight);
+
+		changeSelection();
+		ClientPrefs.saveSettings();
+
+		super.create();
+	}
+
+	override function closeSubState() {
+		super.closeSubState();
+		ClientPrefs.saveSettings();
+	}
+
+	override function update(elapsed:Float) {
+		super.update(elapsed);
+
+		optionsDisplacementFilter.scaleX = FlxG.random.float(-10, 10);
+		optionsDisplacementFilter.mapPoint = new Point(0, FlxG.random.float(-15, 15));
+
+		if (controls.UI_UP_P) {
+			changeSelection(-1);
+		}
+		if (controls.UI_DOWN_P) {
+			changeSelection(1);
+		}
+
+		if (controls.BACK) {
+			FlxG.sound.play(Paths.sound('cancelMenu'));
+			MusicBeatState.switchState(new MainMenuState());
+		}
+
+		if (controls.ACCEPT) {
+			openSelectedSubstate(options[curSelected]);
+		}
+	}
+	
+	function changeSelection(change:Int = 0) {
+		curSelected += change;
+		if (curSelected < 0)
+			curSelected = options.length - 1;
+		if (curSelected >= options.length)
+			curSelected = 0;
+
+		var bullShit:Int = 0;
+
+		for (item in grpOptions.members) {
+			item.targetY = bullShit - curSelected;
+			bullShit++;
+
+			item.alpha = 0.6;
+			if (item.targetY == 0) {
+				item.alpha = 1;
+				selectorLeft.x = item.x - 63;
+				selectorLeft.y = item.y;
+				selectorRight.x = item.x + item.width + 15;
+				selectorRight.y = item.y;
+			}
+		}
+		FlxG.sound.play(Paths.sound('scrollMenu'));
+	}
+}
